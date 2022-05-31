2f5e20a970b88e1ec7f025f516a1b62cfb225e35
yamadamasashi <shisama94@gmail.com>
2022-05-31 09:22:29 +0900

    謎エラー対応

--- nextjs-recoil/src/pages/_app.tsx
+++ nextjs-recoil/src/pages/_app.tsx
@@ -1,12 +1,15 @@
-import { RecoilRoot } from 'recoil'
-import '../styles/globals.css'
+
+import { RecoilRoot } from 'recoil';
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <RecoilRoot>
       <Component {...pageProps} />
-    </RecoilRoot>)
+    </RecoilRoot>
+  )
 }
 
 export default MyApp
+
+// https://forum.onflow.org/t/this-component-must-be-used-inside-a-recoilroot-component/911 これを試してみる
